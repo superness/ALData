d5fd8f62100bcf641313abd2205aa59662a91314
superness <super.hero.excuse@gmail.com>
2020-10-14 21:06:05 -0700

    Add files via upload

---
+++ aldata.js
@@ -0,0 +1,47 @@
+function PostServerStatus()
+{
+	let statuses = Object.keys(parent.S).map(e => 
+	{
+		parent.S[e].eventname = e
+		parent.S[e].server_region = parent.server_region
+		parent.S[e].server_identifier = parent.server_identifier
+
+		return parent.S[e]
+	})
+
+	for(let status of statuses)
+	{
+		let xhr = new XMLHttpRequest();
+		xhr.open("POST", "https://aldata.info/api/serverstatus", true);
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.send(JSON.stringify(status));
+	}
+}
+
+// GetServerStatuses(s => { show_json(s) });
+// GetServerStatuses(s => { 
+// 	let liveEvents = s.filter(e => true == e.live)
+
+// 	for(let event of liveEvents)
+// 	{
+// 		let eventMsg = event.eventname + " live @ " + event.server_region + " " + event.server_identifier
+// 		if(null != event.target)
+// 		{
+// 			eventMsg += " fighting " + event.target
+// 		}
+// 		game_log(eventMsg)
+// 	}
+// });
+function GetServerStatuses(callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(JSON.parse(xmlHttp.responseText));
+    }
+    xmlHttp.open("GET", "https://www.aldata.info/api/ServerStatus", true); // true for asynchronous 
+	xmlHttp.send(null);
+}
+
+PostServerStatus()
+setInterval(PostServerStatus, 1000 * 5) // post updates
