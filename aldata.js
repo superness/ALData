cace8628edb80eae058b535c6fed657d31e30b47
superness <super.hero.excuse@gmail.com>
2020-12-08 07:24:47 -0800

    Add npc info updates

--- aldata.js
+++ aldata.js
@@ -15,6 +15,35 @@ function PostServerStatus()
     xhr.send(JSON.stringify(statuses));
 }
 
+function PostNPCStatus()
+{
+    // Find any notable NPCs around us to post
+    let notableNpcs = ["Kane", "Angel"]
+    let npcInfos = []
+
+    for(npc of notableNpcs) {
+        let foundEntity = get_entity(npc)
+
+        if(null != foundEntity) {
+            npcInfos.push({
+                server_region: parent.server_region,
+                server_identifier: parent.server_identifier,
+                map: foundEntity.map,
+                x: foundEntity.x,
+                y: foundEntity.y,
+                name: npc
+            })
+        }
+    }
+
+    if(npcInfos.length > 0) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", "https://aldata.info/api/npcinfos", true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify(npcInfos));
+    }
+}
+
 // GetServerStatuses(s => { show_json(s) });
 // GetServerStatuses(s => { 
 // 	let liveEvents = s.filter(e => true == e.live)
@@ -40,5 +69,30 @@ function GetServerStatuses(callback)
     xmlHttp.send(null);
 }
 
+function GetNPCInfosForServer(server_region, server_identifier, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(JSON.parse(xmlHttp.responseText));
+    }
+
+    xmlHttp.open("GET", `https://www.aldata.info/api/NPCInfo/${server_region}/${server_identifier}`, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
+function GetNPCInfos(callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(JSON.parse(xmlHttp.responseText));
+    }
+    xmlHttp.open("GET", "https://www.aldata.info/api/NPCInfo", true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+
 PostServerStatus()
+PostNPCStatus()
 setInterval(PostServerStatus, 1000 * 5) // post updates
+setInterval(PostNPCStatus, 1000 * 5) // post updates
